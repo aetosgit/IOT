ce02a5cecace781bf6e5db9cf74264201aa54ab0
aetosgit <132659526+aetosgit@users.noreply.github.com>
2023-07-17 12:10:28 +0530

    Add files via upload

---
+++ Setup_Webserver_ESP32_Intro.ino
@@ -0,0 +1,57 @@
+/* ESP32 Web Server in STA Mode modified on 4th July 2023 
+by Venkat @ AETOS */
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+// SSID & Password
+const char* ssid = "Mr Jasper";  // Enter your SSID here
+const char* password = "Danush21!";  //Enter your Password here
+
+WebServer server(80);  // Object of WebServer(HTTP port, 80 is defult)
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Try Connecting to ");
+  Serial.println(ssid);
+
+  // Connect to your wi-fi modem
+  WiFi.begin(ssid, password);
+
+  // Check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected successfully");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
+
+  server.on("/", handle_root);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  delay(100); 
+}
+
+void loop() {
+  server.handleClient();
+}
+
+// HTML & CSS contents which display on web server
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<h1>My First Web Server with ESP32 - Station Mode &#128522;</h1>\
+<h2>Created at T.I School ATL IoT Lab</h2>\
+<h3>by 8th Grade IoT experts </h3>\
+</body>\
+</html>";
+
+// Handle root url (/)
+void handle_root() {
+  server.send(200, "text/html", HTML);
+}
